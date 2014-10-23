9cc53c5b22ffebbd1aa444f4c62f3514182c7cc7
ablagoev <ablagoev@xs-software.com>
2014-10-23 18:43:57 +0300

    Fix deprecation and output issue for rspec 2.99

--- lib/spork/test_framework/rspec.rb
+++ lib/spork/test_framework/rspec.rb
@@ -7,6 +7,10 @@ class Spork::TestFramework::RSpec < Spork::TestFramework
       ::Spec::Runner::CommandLine.run(
         ::Spec::Runner::OptionParser.parse(argv, stderr, stdout)
       )
+    elsif rspec299?
+      options = ::RSpec::Core::ConfigurationOptions.new(argv)
+      options.parse_options
+      ::RSpec::Core::Runner.new(options).run(stderr, stdout)
     else
       ::RSpec::Core::CommandLine.new(argv).run(stderr, stdout)
     end
@@ -14,5 +18,10 @@ class Spork::TestFramework::RSpec < Spork::TestFramework
 
   def rspec1?
     defined?(Spec) && !defined?(RSpec)
+  end
+
+  def rspec299?
+    return false if !defined?(::RSpec::Core::Version::STRING)
+    ::RSpec::Core::Version::STRING =~ /^2\.99/
   end
 end
